eac7fd34a6364ba7488682dac6a874a0ff77c7f2
Harrison <h.mesplay@gmail.com>
2026-04-14 12:17:54 -0400

    4/14

---
+++ M10ToDoList/ListViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  M10ToDoList
+//
+//  Created by Mesplay, Harrison W. on 4/14/26.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+    
+    var toDoList = [String]()
+
+    @IBOutlet weak var toDoListTableView: UITableView!
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let vc = segue.destination.children.first as? AddViewController{
+            vc.listVC = self
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
+extension ListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return toDoList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = toDoList[indexPath.row]
+        return cell
+    }
+    
+    
+}
